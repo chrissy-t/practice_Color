bcafb6fefc1fbbe5d37f1553d9d871554e0de2b9
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2022-02-11 12:15:55 -0900

    change class name

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ Each grid cell should have an ID matching its colour (red, orange, yellow, green
 Bind an event listener to the grid and use event delegation to determine which colour was clicked.
 Place a message below the grid which asks the user to click on a particular colour and once clicked, 
 update the message to let them know if they were correct or not */
+
+var gridContainer= document.querySelector('.gridContainer')
+
+gridContainer.addEventListener('click', function(e){
+
+})
+
+var colorsArray = ["red", "orange", "yellow", "green", "blue", "violet"]
+
+
+function getRandomColor(){
+var random = Math.floor(Math.random() * colorsArray.length)
+return colorsArray[random]
+}
