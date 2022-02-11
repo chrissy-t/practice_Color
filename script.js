9a985c5e103970a7d1a638bd6806c8a5fbdfaac1
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2022-02-11 11:23:38 -0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/*To practice JavaScript events, create a webpage which can help people check if they have colour blindness.
+ Create a grid of the colours of the rainbow that the user can select from.
+Each grid cell should have an ID matching its colour (red, orange, yellow, green, blue, indigo or violet).
+Bind an event listener to the grid and use event delegation to determine which colour was clicked.
+Place a message below the grid which asks the user to click on a particular colour and once clicked, 
+update the message to let them know if they were correct or not */
